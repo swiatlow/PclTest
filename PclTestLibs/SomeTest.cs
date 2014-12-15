cb2cd6d4096bc5f300bc4fc3e265dd1a4d6b9b6d
lswiatlowski <lswiatlowski@kamsoft.pl>
2014-12-15 08:31:07 +0100

    dodane referencje do nuget'a + wywolanie json'a

--- PclTestLibs/SomeTest.cs
+++ PclTestLibs/SomeTest.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,7 +10,8 @@ namespace PclTestLibs
     {
         public string Test()
         {
-            return "test string";
+            var json = JsonConvert.SerializeObject("test string");
+            return json;
         }
     }
 }
