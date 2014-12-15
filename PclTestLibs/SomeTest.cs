fad2769a6313a475849593a7c6acd734dd01a110
lswiatlowski <lswiatlowski@kamsoft.pl>
2014-12-15 08:27:55 +0100

    utworzony testowy pcl

---
+++ PclTestLibs/SomeTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PclTestLibs
+{
+    public class SomeTest
+    {
+        public string Test()
+        {
+            return "test string";
+        }
+    }
+}
